b93d4fa3f8a6fa2007307507b93f6aba5897b5bf
bigDeal <ferimanandhar@gmail.com>
2017-04-05 10:43:30 +0545

    gulpfile.js changed

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+//concatenate files even located at different locations
 var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
@@ -10,33 +10,20 @@ gulp.task('sass',function(){
 	return gulp.src('resources/assets/sass/app.scss')
 	.pipe(sass())
 	.pipe(gulp.dest('public/css'))
-	 // .pipe(browserSync.reload({
-  //     stream: true
-  //   }))
+
 });
 
-gulp.task('watch',['browserSync','sass'],function(){
+gulp.task('watch',['sass'],function(){
 	gulp.watch('resources/assets/sass/app.scss',['sass']);
-	// gulp.watch('public/**/*.js',browserSync.reload)
 });
 
-// gulp.task('browserSync', function() {
-//   browserSync.init({
-//     proxy:'http://localhost:8000',
-//     server: {
-//       baseDir: 'public/css/app.css'
-//     },
-//   })
-// });
 
-
-
-//gulp.task('useref', function(){
-  //return gulp.src('app/*.html')
-    //.pipe(useref())
-    // Minifies only if it's a JavaScript file
-    //.pipe(gulpIf('*.js', uglify()))
+gulp.task('useref', function(){
+  return gulp.src('resources/views/test.blade.php')
+    .pipe(useref())
+    //Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
     //minifies only css
-    //.pipe(gulpIf('*.css', cssnano()))
-  //  .pipe(gulp.dest('dist'))
-//});
+    .pipe(gulpIf('*.css', cssnano()))
+   .pipe(gulp.dest('public'))
+});
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,9 +1,18 @@
 <html>
 <head>
-    <title></title>
-    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+	<title></title>
+
+	<link rel="stylesheet" href="{{mix('css/app.css')}}">
+
+	<!--build:css css/test.min.css-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css.map">
+	<link rel="stylesheet" href="
+	https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap-theme.min.css
+	">
+	<!-- endbuild-->
+
 </head>
 <body>
-<h1>Test</h1>
+	<h1>Test</h1>
 </body>
 </html>
